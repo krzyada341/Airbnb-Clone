616b80fd6bdf442248fc4d541ead74469da5d4c7
krzyada341 <krzyada341@student.polsl.pl>
2026-01-29 18:45:21 +0100

    Created navbar components with Auth with next/auth

---
+++ app/components/Button.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { IconType } from 'react-icons'
+
+interface ButtonProps {
+	label: string
+	onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+	disabled?: boolean
+	outline?: boolean
+	small?: boolean
+	icon?: IconType
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick, disabled, outline, small, icon: Icon }) => {
+	const outlineStyles = outline ? 'bg-white border-black text-black' : 'bg-rose-500 border-rose-500 text-white'
+	const smallStyles = small ? 'py-1 text-sm font-light border-[1px]' : 'py-3 text-md font-semibold border-2'
+	return (
+		<button
+			onClick={onClick}
+			disabled={disabled}
+			className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full ${outlineStyles} ${smallStyles}`}>
+			{Icon && <Icon size={24} className="absolute left-4 top-3" />}
+			{label}
+		</button>
+	)
+}
+export default Button
