17e0b70d08ab6faab6b4e406d8fa265c8986cf5c
krzyada341 <krzyada341@student.polsl.pl>
2026-01-29 20:26:39 +0100

    Added categories UI

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { signIn } from 'next-auth/react'
-import axios from 'axios'
 import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
 import useRegisterModal from '@/app/hooks/useRegisterModal'
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,6 +5,15 @@ import { useRouter } from 'next/navigation'
 
 const Logo = () => {
 	const router = useRouter()
-	return <Image alt="Logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/logo.png" />
+	return (
+		<Image
+			onClick={() => router.push('/')}
+			alt="Logo"
+			className="hidden md:block cursor-pointer"
+			height="100"
+			width="100"
+			src="/images/logo.png"
+		/>
+	)
 }
 export default Logo
