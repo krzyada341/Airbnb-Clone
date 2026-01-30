27f07b482739f394b886296e7643d47338de96db
krzyada341 <krzyada341@student.polsl.pl>
2026-01-30 23:51:10 +0100

    Added home page listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-	return <div className="text-rose-500 text-2xl">Heloo Airbnb</div>
+import { Listing } from '@prisma/client'
+import getCurrentUser from './actions/getCurrentUser'
+import getListings from './actions/getListing'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
+import ListingCard from './components/listings/ListingCard'
+
+export default async function Home() {
+	const listings = await getListings()
+	const currentUser = await getCurrentUser()
+
+	if (listings.length === 0) {
+		return <EmptyState showReset />
+	}
+
+	return (
+		<Container>
+			<div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+				{listings.map((listing: Listing) => {
+					return <ListingCard key={listing.id} currentUser={currentUser} data={listing} />
+				})}
+			</div>
+		</Container>
+	)
 }
