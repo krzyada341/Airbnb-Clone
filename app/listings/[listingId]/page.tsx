ba043002e963f35dae74042995d846f290c438a7
krzyada341 <krzyada341@student.polsl.pl>
2026-01-31 18:03:17 +0100

    Added listing  component with reservation functionality

---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,25 @@
+import getCurrentUser from '@/app/actions/getCurrentUser'
+import getListingById from '@/app/actions/getListingById'
+import ListingClient from '@/app/listings/[listingId]/ListingClient'
+import EmptyState from '@/app/components/EmptyState'
+import getReservations from '@/app/actions/getReservations'
+
+interface IParams {
+	listingId?: string
+}
+
+const ListingPage = async ({ params }: { params: Promise<IParams> }) => {
+	const listing = await getListingById(params)
+	const reservations = await getReservations(params)
+	const currentUser = await getCurrentUser()
+
+	if (!listing) {
+		return <EmptyState />
+	}
+	return (
+		<div>
+			<ListingClient listing={listing} reservations={reservations} currentUser={currentUser} />
+		</div>
+	)
+}
+export default ListingPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Listing } from '@prisma/client'
 import getCurrentUser from './actions/getCurrentUser'
 import getListings from './actions/getListing'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState'
 import ListingCard from './components/listings/ListingCard'
+import { SafeListing } from './types'
 
 export default async function Home() {
 	const listings = await getListings()
@@ -16,7 +16,7 @@ export default async function Home() {
 	return (
 		<Container>
 			<div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-				{listings.map((listing: Listing) => {
+				{listings.map((listing: SafeListing) => {
 					return <ListingCard key={listing.id} currentUser={currentUser} data={listing} />
 				})}
 			</div>
